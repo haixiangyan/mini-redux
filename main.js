2ef57c30cb9870099e65b1bf0d6553ba5167ccfd
Haixiang <haixiang6123@gmail.com>
2020-08-25 22:04:04 +0800

    feat: 实现 dispatch 函数

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-const createStore = () => {
-  let currentState = {}
+let initState = {
+  count: 1
+}
+
+const createStore = (initState) => {
+  let currentState = initState
 
   const getState = () => currentState
 
-  const dispatch = () => {}
+  const dispatch = (action) => {
+    switch (action.type) {
+      case 'plus':
+        currentState = {
+          ...currentState,
+          count: currentState.count + action.payload
+        }
+    }
+  }
 
   const subscribe = () => {}
 }
